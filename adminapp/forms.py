6d4210209ace6ec04962a407f8280c8bc9f872f0
DxTTU <dhatrinathlade2006@gmail.com>
2024-11-04 13:22:31 +0530

    Dattu

---
+++ adminapp/forms.py
@@ -0,0 +1,17 @@
+import pytz
+from django import forms
+from .models import Task, StudentList
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model=Task
+        fields=['title']
+
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = StudentList
+        fields = ['Register_Number', 'Name']
+
